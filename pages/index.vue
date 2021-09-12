3c81d7388a9f17085d81e152512b35761eb72485
Kento Shirasawa <shirasawa.kento@gmail.com>
2021-09-12 15:28:23 +0900

    added some files

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,21 @@
 <template>
-  <Tutorial />
+  <div>
+    <nuxt-link
+      to="/page2"
+    >
+      Go to Page2
+    </nuxt-link>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style lang="scss" scoped>
+a {
+  background: #000;
+  color: #fff;
+  padding: 1rem;
+}
+</style>
---
+++ pages/page2.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Page2</h1>
+    <nuxt-link
+      to="/"
+    >
+      Go to Index
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
