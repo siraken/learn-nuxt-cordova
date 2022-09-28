560874f478ab75bc8c50d5446bacc644a6317c43
Kento Shirasawa <shirasawa.kento@gmail.com>
2022-09-29 01:25:31 +0900

    chore: package updated, or changed etc.

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,55 +13,33 @@ export default {
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'initial-scale=1, width=device-width, viewport-fit=cover' },
+      {
+        name: 'viewport',
+        content: 'initial-scale=1, width=device-width, viewport-fit=cover'
+      },
       { name: 'msapplication-tap-highlight', content: 'no' },
       { name: 'color-scheme', content: 'light dark' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ],
-    script: [
-      { src: 'cordova.js', body: true },
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    script: [{ src: 'cordova.js', body: true }]
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '@/assets/scss/app.scss'
-  ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
+  css: ['@/assets/scss/app.scss'],
+  plugins: [],
   components: true,
-
-  // Transition
   transition: 'page',
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-    // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss'
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios'
-  ],
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  buildModules: ['@nuxtjs/eslint-module', '@nuxt/postcss8'],
+  modules: ['@nuxtjs/axios'],
   axios: {},
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    publicPath: '/nuxt/'
+    publicPath: '/nuxt/',
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {}
+      }
+    }
   },
 
   // Router
